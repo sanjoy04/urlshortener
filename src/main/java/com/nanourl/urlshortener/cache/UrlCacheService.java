9ed3345dd9156acb080f0b0912c34879fa004bdc
sanjoy <sanjoy12k@gmail.com>
2026-03-15 23:37:45 +0530

    Implemented distributed ID generation using SnowFlake and added url expiry as well

--- src/main/java/com/nanourl/urlshortener/cache/UrlCacheService.java
+++ src/main/java/com/nanourl/urlshortener/cache/UrlCacheService.java
@@ -4,17 +4,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import lombok.RequiredArgsConstructor;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @RequiredArgsConstructor
 public class UrlCacheService {
 
     private final StringRedisTemplate redisTemplate;
 
-    public void save(String shortCode, String longUrl) {
-        redisTemplate.opsForValue().set(shortCode, longUrl);
+    public void save(String shortCode, String longUrl, long ttlSeconds) {
+        redisTemplate.opsForValue().set("url:"+shortCode, longUrl, ttlSeconds, TimeUnit.SECONDS);
     }
 
     public String get(String shortCode) {
-        return redisTemplate.opsForValue().get(shortCode);
+        return redisTemplate.opsForValue().get("url:"+shortCode);
+    }
+
+    public void remove(String shortCode) {
+        redisTemplate.delete("url:"+shortCode);
     }
 }
